de0908560bec11432c68435d937d0fc2e8df0242
Shawn Coverini <coverinis@gmail.com>
2019-02-10 17:49:17 -0500

    Door Animation
    Door Animation works
    Door closes on trigger

---
+++ ECE_VR_Self_Regulation/Assets/Scripts/CloseDoor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDoor : MonoBehaviour
+{
+    public Animator animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetTrigger("Close");
+    }
+}
---
+++ ECE_VR_Self_Regulation/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Animator door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        door.SetTrigger("Open");
+    }
+
+}
