a9fd34848d1e7e43a67317a0643036483fdd9b5b
Shawn Coverini <coverinis@gmail.com>
2019-02-10 03:31:09 -0500

    Update SteanVR SDK
    Update to SteamVR 2.2
    Finish Proper animation

---
+++ ECE_VR_Self_Regulation/Assets/OpenCloseDoor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class OpenCloseDoor : MonoBehaviour
+{
+
+    public Animator Animator { get => animator; }
+    public string Open { get => open; }
+    public string Close { get => close; }
+    public int time;
+
+    private Animator animator;
+    private string open;
+    private string close;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        open = "Open";
+        close = "Close";
+        animator.SetTrigger(Open);
+        Invoke("trigerClose", time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void trigerClose()
+    {
+        animator.SetTrigger(Close);
+    }
+}
+
+[CustomEditor(typeof(OpenCloseDoor))]
+public class OpenCloseDoorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        OpenCloseDoor openCloseDoor = (OpenCloseDoor)target;
+        openCloseDoor.time = EditorGUILayout.IntField("Time", openCloseDoor.time);
+
+        this.name = "Open Close";
+    }
+}
