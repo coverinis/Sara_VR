6d9fbb90a3547bb5694b6ef71bb8e6eed1f0b321
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2019-03-08 11:37:13 -0500

    initial working project moved here
    Project was built in different repositorie but moved here to share with other team memeber of ECE VR Self Regulation (capston project) team. From here this project is ready to be improve, test and integarte with other projects

---
+++ IBM_Watson/ECE_Text_Analyzer/ECE_Text_Analyzer/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.DataVisualization.Charting;
+using IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.Example;
+using IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.Model;
+using System.ComponentModel;
+
+namespace ECE_Text_Analyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Thread AnalyzerThread;
+        private static MainWindow window;
+        public MainWindow()
+        {
+            InitializeComponent();
+            window = this;
+            AnalyzerThread = new Thread(Example.Satrt);
+            AnalyzerThread.Start();
+        }
+
+        public static void UpdateSetimentLable(DocumentSentimentResults sentimentResults)
+        {
+            window.Dispatcher.Invoke(()=>
+            {
+                window.lb_sentiLable.Content = $"Sentiment: {sentimentResults.Label.ToUpperInvariant()}";
+                if (sentimentResults.Score > 0)
+                {
+                    window.lb_sentiLable.Foreground = Brushes.Green;
+                }
+                else window.lb_sentiLable.Foreground = Brushes.Red;
+            });
+        }
+
+        public static void UpdateEmotionChart(DocumentEmotionResults emotionResults)
+        {
+            window.Dispatcher.Invoke(() =>
+            {
+                window.cs_Joy.ItemsSource = new KeyValuePair<string, int>[]
+                {
+                    new KeyValuePair<string, int>("Joy", (int)(emotionResults.Emotion.Joy*100))
+                };
+                window.cs_Anger.ItemsSource = new KeyValuePair<string, int>[]
+                {
+                    new KeyValuePair<string, int>("Anger", (int)(emotionResults.Emotion.Anger*100))
+                };
+                window.cs_Disgust.ItemsSource = new KeyValuePair<string, int>[]
+                {
+                    new KeyValuePair<string, int>("Disgust", (int)(emotionResults.Emotion.Disgust*100))
+                };
+                window.cs_Sadness.ItemsSource = new KeyValuePair<string, int>[]
+                {
+                    new KeyValuePair<string, int>("Sadness", (int)(emotionResults.Emotion.Sadness*100))
+                };
+                window.cs_Fear.ItemsSource = new KeyValuePair<string, int>[]
+                {
+                    new KeyValuePair<string, int>("Fear", (int)(emotionResults.Emotion.Fear*100))
+                };
+            });
+            
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            AnalyzerThread.Abort();
+            base.OnClosing(e);
+        }
+    }
+}
---
+++ IBM_Watson/ECE_Text_Analyzer/ECE_Text_Analyzer/NaturalLanguageUnderstandingExample.cs
@@ -0,0 +1,67 @@
+﻿/**
+* Copyright 2017 IBM Corp. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+using System;
+using IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.Model;
+using ECE_Text_Analyzer;
+
+
+namespace IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.Example
+{
+    public class NaturalLanguageUnderstandingExample
+    {
+        private NaturalLanguageUnderstandingService _naturalLanguageUnderstandingService;
+
+        #region Constructor
+        public NaturalLanguageUnderstandingExample(string url, string apiKey)
+        {
+            Util.TokenOptions token = new Util.TokenOptions()
+            {
+                IamApiKey = apiKey
+            };
+            
+            _naturalLanguageUnderstandingService = new NaturalLanguageUnderstandingService(token, "2018-12-19");
+            //_naturalLanguageUnderstandingService.SetEndpoint(url);
+        }
+        #endregion
+
+        #region Analyze
+        public void Analyze(string textToAnalyze)
+        {
+            Parameters parameters = new Parameters()
+            {
+                Text = textToAnalyze,
+                Features = new Features()
+                {
+                    Emotion = new EmotionOptions()
+                    {
+                        Document = true
+                    },
+                    Sentiment = new SentimentOptions()
+                    {
+                        Document = true
+                    }
+                }
+            };
+            var result = _naturalLanguageUnderstandingService.Analyze(parameters);
+            MainWindow.UpdateSetimentLable(result.Sentiment.Document);
+            MainWindow.UpdateEmotionChart(result.Emotion.Document);
+
+            
+        }
+        #endregion
+    }
+}
