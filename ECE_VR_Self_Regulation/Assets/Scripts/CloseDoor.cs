84ca58be2973d3fb1d63075d5321e70d49dadc42
Shawn Coverini <coverinis@gmail.com>
2019-03-11 09:34:02 -0400

    Fix Close door

--- ECE_VR_Self_Regulation/Assets/Scripts/CloseDoor.cs
+++ ECE_VR_Self_Regulation/Assets/Scripts/CloseDoor.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class CloseDoor : MonoBehaviour
 {
     public Animator animator;
+    public OpenDoor openDoor; 
+
+    private void Start()
+    {
+        openDoor.enabled = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Player"))
         {
             animator.SetTrigger("Close");
-            gameObject.SetActive(false);
+            this.gameObject.SetActive(false);
         }
     }
 }
